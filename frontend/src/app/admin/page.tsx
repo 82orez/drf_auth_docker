3383421355188e206b2666f4dda6734b85b27b18
82orez <82orez@naver.com>
2025-11-22 08:24:39 +0900

    docker superuser frontend added-1

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -5,10 +5,11 @@ import { useRouter } from "next/navigation";
 
 const AdminRedirect = () => {
   const router = useRouter();
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
 
   useEffect(() => {
     // Django admin 페이지로 리다이렉트
-    window.location.href = "http://localhost:8000/admin/";
+    window.location.href = `${API_BASE_URL}/admin`;
   }, []);
 
   return (
