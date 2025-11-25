fcc24db92028814426dc9c1d0ebdf08b1fd5a936
82orez <82orez@naver.com>
2025-11-26 07:28:37 +0900

    session cookie settings changed-6 static files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -110,7 +110,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # collectstatic 결과물
+
+# 업로드 파일
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
