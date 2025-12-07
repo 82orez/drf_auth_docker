9fcf9adecdd558a53e77bee19480a8023316ba3d
82orez <82orez@naver.com>
2025-12-08 07:04:59 +0900

    admin page text changed

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,6 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+# admin page 텍스트 설정
+admin.site.site_header = "Friending administration"  # 상단 왼쪽 큰 제목
+admin.site.site_title = "Friending Admin"  # 브라우저 탭 제목(<title>)
+# admin.site.index_title = "관리자 대시보드"  # 메인 화면의 제목
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
